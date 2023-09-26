708940c4bd6df31c8b59a9f346cb22fb2148aea1
Adam <akhrorkuloww@gmail.com>
2023-09-26 12:14:14 +0500

    Add project files.

---
+++ FactoryPattern/Factories/GenerateClassWithDataFactory.cs
@@ -0,0 +1,37 @@
+﻿using FactoryPattern.Samples;
+
+namespace FactoryPattern.Factories;
+
+public static class GenerateClassWithDataFactoryExtension
+{
+    //AddGenericClassWithDataFactory
+    public static void AddGenericClassWhithFactory(this IServiceCollection services)
+    {
+        services.AddTransient<IUserData, UserData>();
+        services.AddSingleton<Func<IUserData>>(x => () => x.GetService<IUserData>()!);
+        services.AddSingleton<IUserDataFactory, UserDataFactory>();
+    }
+
+}
+
+public interface IUserDataFactory
+{
+    IUserData Crete(string name);
+}
+
+public class UserDataFactory : IUserDataFactory
+{
+    private readonly Func<IUserData> _factory;
+
+    public UserDataFactory(Func<IUserData> factory)
+    {
+        _factory = factory;
+    }
+
+    public IUserData Crete(string name)
+    {
+        var output = _factory();
+        output.Name = name;
+        return output;
+    }
+}
